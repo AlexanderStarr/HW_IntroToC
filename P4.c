42dffe301399326aef97005645c8f2e1c0b2047d
Alexander Starr <AlexanderLStarr@gmail.com>
2014-02-09 00:50:41 -0600

    Bubble sort seems to be working now.

--- P4.c
+++ P4.c
@@ -2,16 +2,22 @@
 #include <stdlib.h>
 #include "link.h"
 
+// Adds a new element with the supplied value to the end of
+// the linked list supplied by the pointer.
 void add(slist_t *list, int value) {
+    // Allocate memory and initialize the list item.
     list_item_t *item = (list_item_t *) malloc(sizeof(list_item_t));
     item->value = value;
     item->next = NULL;
+    // If the list is empty, make the new item the head and tail.
     if(list->head == NULL) {
         //printf("NULL head\n");
         list->head = item;
         list->tail = item;
         item->prev = NULL;
     } else {
+        // Otherwise, make the element the new tail and
+        // rearrange the pointers appropriately.
         //printf("Adding to %d\n", list->tail->value);
         item->prev = list->tail;
         list->tail->next = item;
@@ -19,6 +25,7 @@ void add(slist_t *list, int value) {
     }
 }
 
+// Prints the supplied linked list from head to tail.
 void print(slist_t *list) {
     printf("List: ");
     list_item_t *iter = list->head;
@@ -29,6 +36,8 @@ void print(slist_t *list) {
     printf("\n");
 }
 
+// Removes the elements of the supplied list.  Frees the memory
+// used by these items as well.
 void empty(slist_t *list) {
     list_item_t *prev = list->head;
     list_item_t *iter = prev;
@@ -41,16 +50,24 @@ void empty(slist_t *list) {
     }
 }
 
+// Handles rearranging pointers to properly swap two elements.
+// item1 must occur directly before item2 in the list.
 void swap(slist_t *list, list_item_t *item1, list_item_t *item2) {
+    // First move the pointers rooted at the two elements.
     item1->next = item2->next;
     item2->next = item1;
     item2->prev = item1->prev;
     item1->prev = item2;
+    // If the now-first element has no predecessor, then it must
+    // be the head element.  Otherwise, change its predecessor's
+    // next field to now point to the right element.
     if(item2->prev == NULL) {
         list->head = item2;
     } else {
         item2->prev->next = item2;
     }
+    // If the now-second element has no next, then it must be the
+    // tail element. Otherwise, carrect the next element's prev field.
     if(item1->next == NULL) {
         list->tail = item1;
     } else {
@@ -58,12 +75,27 @@ void swap(slist_t *list, list_item_t *item1, list_item_t *item2) {
     }
 }
 
+// Sorts the supplied list using the bubble sort algorithm.
 void bublesort(slist_t *list) {
-    list_item_t *iter1 = list->head;
-    list_item_t *iter2 = iter1;
+    list_item_t *iter1;
+    list_item_t *iter2;
     int isSorted = 0;
     while(isSorted == 0) {
-
+        isSorted = 1;
+        iter1 = list->head;
+        iter2 = iter1->next;
+        while(iter2 != NULL) {
+            if(iter1->value > iter2->value) {
+                isSorted = 0;
+                swap(list, iter1, iter2);
+                // iter1 is now the latter element, iter2 the earlier.
+                // So they must be advanced accordingly.
+                iter2 = iter1->next;
+            } else {
+                iter1 = iter2;
+                iter2 = iter2->next;
+            }
+        }
     }
 }
 
@@ -71,11 +103,11 @@ int main() {
     slist_t myList;
     print(&myList);
     add(&myList, 4);
+    add(&myList, 5);
     add(&myList, 2);
-    add(&myList, 3);
     add(&myList, 1);
     print(&myList);
-    swap(&myList, myList.head->next, myList.head->next->next);
+    bublesort(&myList);
     print(&myList);
     return 0;
 }
