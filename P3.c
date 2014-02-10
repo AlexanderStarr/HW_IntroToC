c1893902511fcdb4d702882b1f872a889bf82112
Alexander Starr <AlexanderLStarr@gmail.com>
2014-02-09 18:18:23 -0600

    Added some more comments.

--- P3.c
+++ P3.c
@@ -4,6 +4,7 @@
 
 #define LINE_LEN 100
 
+// Trims the newline character from a string.
 void trim(char *string) {
     size_t len = strlen(string) - 1;
     if(string[len] == '\n') string[len] = '\0';
@@ -19,31 +20,34 @@ int main() {
     char read_line[LINE_LEN], search_line[LINE_LEN];
     char *item_ptr, *search_ptr;
 
-    // Read all the lines in wiki.txt.
+    // Read, trim, and copy all the lines in wiki.txt.
     while(fgets(read_line, sizeof(read_line), wiki) != NULL) {
         trim(read_line);
         // For each read_line, copy elements 1 and 2.
-        // strtok returns a ptr to elements, and advances when called with NULL.
+        // strtok returns a ptr to elements delimited by ","
+        // and advances to the next one when called with NULL.
         item_ptr = strtok(read_line, ",");
         for (int i=0; i<2; i++) {
             fprintf(output, "%s,", item_ptr);
             item_ptr = strtok(NULL, ",");
         }
-        // Now we need to find the line in authors.txt that starts
-        // with the string item_ptr and copy it into output.
+        // item_ptr now points to the author ID.
+        // Find the line in authors.txt with that ID and copy it into output.
         while(fgets(search_line, sizeof(search_line), authors) != NULL) {
             trim(search_line);
             search_ptr = strtok(search_line, ",");
-            //printf("Searching for %s, found %s", item_ptr, search_ptr);
+            // If search_ptr == item_ptr, the author IDs match and we copy.
             if(strcmp(item_ptr, search_ptr) == 0) {
-                printf("Match found/n");
                 search_ptr = strtok(NULL, ",");
                 fprintf(output, "%s,", search_ptr);
                 search_ptr = strtok(NULL, ",");
-                fprintf(output, "%s\n", search_ptr);
+                fprintf(output, "%s", search_ptr);
                 break;
             }
         }
+        // Starts a new line even if the auther ID was not found.
+        fprintf(output, "\n");
+        // Start the authors file back at the beginning for the next search.
         authors = fopen("authors.txt", "r");
     }
     return 0;
