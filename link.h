93ebb78fa5fd3862fdbe1c941f2216ac3dc9b4e6
Alexander Starr <AlexanderLStarr@gmail.com>
2014-02-11 11:01:54 -0600

    Updated assignment requirements.

--- link.h
+++ link.h
@@ -1,17 +1,21 @@
+// version 1.1
+// -- added support for initializing the list
+
 typedef struct list_item {
-	int value;
-	struct list_item *next;
-	struct list_item *prev;
+    int value;
+    struct list_item *next;
+    struct list_item *prev;
 } list_item_t; 
 
 /**
 global variable declaration
 **/
 typedef struct single_list {
-	list_item_t *head;
-	list_item_t *tail;
+    list_item_t *head;
+    list_item_t *tail;
 } slist_t;
 
+void init(slist_t *list);
 void add(slist_t *list, int value);
 void print(slist_t *list);
 void empty(slist_t *list);
